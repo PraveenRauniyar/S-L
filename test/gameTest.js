c49f0149a0d6fa0ad4c8afef9469730a738d3a72
PRAVEEN KUMAR GUPTA <guptapraveen@gmail.com>
2017-12-29 01:00:50 +0530

    Added : Test for get random dice value

--- test/gameTest.js
+++ test/gameTest.js
@@ -1,4 +1,4 @@
-let assert = require("assert");
+let assert = require("chai").assert;
 let Game = require("../public/js/game.js");
 let Player = require("../public/js/player.js");
 let SnakeAndLadders = require("../public/js/player.js");
@@ -13,6 +13,12 @@ test["addPlayer() should add player"] = function(){
   assert.equal(game.players[0].name,"Praveen");
   assert.equal(game.players[0].symbol,"*");
 };
+
+test["getRandomDiceValue() should give value between 1 to 6"] = function(){
+  let game = new Game();
+  assert.isAbove(game.getRandomDiceValue(),0);
+  assert.isBelow(game.getRandomDiceValue(),7);
+}
 
 test["updateCurrentPosition() should update current player position"] = function(){
   let game = new Game();
